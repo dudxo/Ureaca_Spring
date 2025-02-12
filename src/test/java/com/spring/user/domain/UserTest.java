9b51c89850f82555f462eebeba557d40e75d8cb5
dudxo <father8475@naver.com>
2025-02-12 18:57:38 +0900

    test: 실패 테스트 disable 처리 및 성공 테스트 케이스 작성

--- src/test/java/com/spring/user/domain/UserTest.java
+++ src/test/java/com/spring/user/domain/UserTest.java
@@ -1,6 +1,7 @@
 package com.spring.user.domain;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,7 +11,19 @@ public class UserTest {
 
 	@DisplayName("[두 회원의 이름이 같다.]")
 	@Test
+	@Disabled
 	void TwoUserIsSameName() {
+	    // given
+		User 철수 = new User("철수");
+		User 영희 = new User("영희");
+
+		// when		// then
+		Assertions.assertEquals(철수.getName(), 영희.getName());
+	}
+
+	@DisplayName("[두 회원의 이름이 같지 않다.]")
+	@Test
+	void TwoUserIsNotSameName() {
 	    // given
 		User 철수 = new User("철수");
 		User 영희 = new User("영희");
