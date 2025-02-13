ac31427f5bfd4fa9e53f54fdd4f93b33d62ac023
dudxo <father8475@naver.com>
2025-02-13 14:02:03 +0900

    chore: start 스크립트 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -16,7 +16,7 @@ cp $JAR_SOURCE $JAR_FILE
 
 # jar 파일 실행
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
-nohup java -jar $PROJECT_ROOT/spring-0.0.1-SNAPSHOT.jar > $APP_LOG 2> $ERROR_LOG &
+nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
 
 CURRENT_PID=$(pgrep -f $JAR_FILE)
 echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
